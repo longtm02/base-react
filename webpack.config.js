8df319df8f715378d458a71e3c2c5918854037bc
Rin <longtm>
2023-07-15 20:06:04 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            compilerOptions: {
+              noEmit: false, // this option will solve the issue
+            },
+            configFile: 'tsconfig.json',
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        include: /public/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        exclude: /\.module\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+        sideEffects: true,
+      },
+
+      {
+        test: /\.module\.(scss|sass)$/ ,
+        exclude: /\.(scss|sass)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              localIdentName: '[local]__[hash:base64:5]',
+              modules: true,
+            },
+          },
+        ],
+        sideEffects: true,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new Dotenv(),
+  ],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  watchOptions: {
+    ignored: ['node_modules', 'dist', 'logs'],
+  },
+};
